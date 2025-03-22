750dc56c98021e4c2e4acb4c2a02bc89b0399323
Orthel Toralen <orthel.toralen@gmail.com>
2025-03-22 01:13:49 -0400

    initial commit

---
+++ models.py
@@ -0,0 +1,126 @@
+import pandas as pd
+from sqlalchemy import String, Integer, DateTime, Float, Boolean, create_engine
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+class MarketStats(Base):
+    __tablename__ = "marketstats"
+    type_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    total_volume_remain: Mapped[int] = mapped_column(Integer, nullable=True)
+    min_price: Mapped[float] = mapped_column(Float, nullable=True)
+    price: Mapped[float] = mapped_column(Float, nullable=True)
+    avg_price: Mapped[float] = mapped_column(Float, nullable=True)
+    avg_volume: Mapped[float] = mapped_column(Float, nullable=True)
+    group_id: Mapped[int] = mapped_column(Integer, nullable=True)
+    type_name: Mapped[str] = mapped_column(String, nullable=True)
+    group_name: Mapped[str] = mapped_column(String, nullable=True)
+    category_id: Mapped[int] = mapped_column(Integer, nullable=True)
+    category_name: Mapped[str] = mapped_column(String, nullable=True)
+    days_remaining: Mapped[float] = mapped_column(Float, nullable=True)
+    last_update: Mapped[DateTime] = mapped_column(DateTime, nullable=True)
+
+
+    def __repr__(self) -> str:
+        f"""marketstats(type_id={self.type_id!r}, 
+        total_volume_remain={self.total_volume_remain!r}, 
+        min_price={self.min_price!r},
+        price_5th_percentile={self.price_5th_percentile!r},
+        avg_of_avg_price={self.avg_of_avg_price!r},
+        avg_daily_volume={self.avg_daily_volume!r},
+        group_id={self.group_id!r},
+        type_name={self.type_name!r},   
+        group_name={self.group_name!r},
+        category_id={self.category_id!r},
+        category_name={self.category_name!r},
+        days_remaining={self.days_remaining!r},
+        last_update={self.last_update!r}
+        """
+
+class MarketOrders(Base):
+    __tablename__ = "marketorders"
+    order_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    is_buy_order: Mapped[bool] = mapped_column(Boolean,nullable=True)
+    type_id: Mapped[int] = mapped_column(Integer,nullable=True)
+    duration: Mapped[int] = mapped_column(Integer,nullable=True)
+    issued: Mapped[DateTime] = mapped_column(DateTime,nullable=True)
+    price: Mapped[float] = mapped_column(Float,nullable=True)
+    volume_remain: Mapped[int] = mapped_column(Integer,nullable=True)
+
+    def __repr__(self) -> str:
+        f"""marketorders(
+        order_id={self.order_id!r},
+        is_buy_order={self.is_buy_order!r},
+        type_id={self.type_id!r},
+        duration={self.duration!r},
+        issued={self.issued!r},
+        location_id={self.location_id!r},
+        min_volume={self.min_volume!r},
+        price={self.price!r},
+        volume_remain={self.volume_remain!r},
+        volume_total={self.volume_total!r},
+        range={self.range!r},
+        order_type={self.order_type!r}
+        )"""
+
+class MarketHistory(Base):
+    __tablename__ = "market_history"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    date: Mapped[DateTime] = mapped_column(DateTime)
+    type_name: Mapped[str] = mapped_column(String(100), nullable=True)
+    type_id: Mapped[str] = mapped_column(String(10))
+    average: Mapped[float] = mapped_column(Float)
+    volume: Mapped[int] = mapped_column(Integer)
+    highest: Mapped[float] = mapped_column(Float)
+    lowest: Mapped[float] = mapped_column(Float)
+    order_count: Mapped[int] = mapped_column(Integer)
+    last_update: Mapped[DateTime] = mapped_column(DateTime, nullable=True)
+
+class Doctrines(Base):
+    __tablename__ = "doctrines"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    fit_id: Mapped[int] = mapped_column(Integer)
+    ship_id: Mapped[int] = mapped_column(Integer, nullable=True)
+    ship_name: Mapped[str] = mapped_column(String, nullable=True)
+    hulls: Mapped[int] = mapped_column(Integer, nullable=True)
+    type_id: Mapped[int] = mapped_column(Integer, nullable=True)
+    type_name: Mapped[str] = mapped_column(String, nullable=True)
+    fit_qty: Mapped[int] = mapped_column(Integer, nullable=True)
+    fits_on_mkt: Mapped[float] = mapped_column(Float, nullable=True)
+    total_stock: Mapped[int] = mapped_column(Integer, nullable=True)
+    four_hour_price: Mapped[float] = mapped_column(Float, nullable=True)
+    avg_vol: Mapped[float] = mapped_column(Float, nullable=True)
+    days: Mapped[float] = mapped_column(Float, nullable=True)
+    group_id: Mapped[int] = mapped_column(Integer, nullable=True)
+    group_name: Mapped[str] = mapped_column(String, nullable=True)
+    category_id: Mapped[int] = mapped_column(Integer, nullable=True)
+    category_name: Mapped[str] = mapped_column(String, nullable=True)
+    timestamp: Mapped[DateTime] = mapped_column(DateTime, nullable=True)
+
+    def __repr__(self) -> str:
+        f"""doctrines(
+        fit_id={self.fit_id!r},
+        ship_id={self.ship_id!r},
+        ship_name={self.ship_name!r},
+        hulls={self.hulls!r},
+        type_id={self.type_id!r},
+        type_name={self.type_name!r},
+        fit_qty={self.fit_qty!r},
+        fits_on_mkt={self.fits_on_mkt!r},
+        total_stock={self.total_stock!r},
+        four_hour_price={self.four_hour_price!r},
+        avg_vol={self.avg_vol!r},
+        days={self.days!r},
+        group_id={self.group_id!r},
+        group_name={self.group_name!r},
+        category_id={self.category_id!r},
+        category_name={self.category_name!r},
+        timestamp={self.timestamp!r}
+        )"""
+
+if __name__ == "__main__":
+    pass
---
+++ turso_handler.py
@@ -0,0 +1,235 @@
+import json
+
+import libsql_experimental as libsql
+import pandas as pd
+import os
+from dotenv import load_dotenv
+from pandas import read_sql_query
+from sqlalchemy import create_engine, select, text, inspect
+from sqlalchemy.orm import Session, base, DeclarativeBase, sessionmaker
+import sqlalchemy_libsql
+import sqlalchemy.dialects.sqlite
+from datetime import timedelta, time, datetime
+import argparse
+
+
+import logging
+
+from models import MarketStats, MarketOrders, Base, MarketHistory
+
+logging.basicConfig(filename='logs/libsqltest.log', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+path_begin = "/mnt/c/Users/User/PycharmProjects"
+
+new_orders = f"{path_begin}/eveESO/output/brazil/new_orders.json"
+new_history = f"{path_begin}//eveESO/output/brazil/new_history.csv"
+new_stats = f"{path_begin}/eveESO/output/brazil/new_stats.csv"
+watchlist = f"{path_begin}/eveESO/output/brazil/watchlist.csv"
+new_orderscsv = f"{path_begin}/eveESO/output/brazil/new_orders.csv"
+new_doctrines = f"{path_begin}/eveESO/output/brazil/new_doctrines.csv"
+
+load_dotenv()
+
+url = os.getenv("TURSO_DATABASE_URL")
+auth_token = os.getenv("TURSO_AUTH_TOKEN")
+
+turso = f"sqlite+{url}/?authToken={auth_token}&secure=true"
+
+sde_url = os.getenv("SDE_URL")
+sde_auth_token = os.getenv("SDE_TOKEN")
+
+sde = f"sqlite+{sde_url}/?authToken={sde_auth_token}&secure=true"
+
+def example(df):
+
+    conn = libsql.connect("wcmkt.db", sync_url=url, auth_token=auth_token)
+    conn.sync()
+
+    # noinspection SqlDialectInspection
+    conn.execute("""CREATE TABLE IF NOT EXISTS marketstats (
+    type_id INTEGER,
+    total_volume_remain INTEGER,
+    min_price FLOAT,
+    price_5th_percentile FLOAT,
+    avg_of_avg_price FLOAT,
+    avg_daily_volume FLOAT,
+    group_id INTEGER,
+    type_name TEXT,
+    group_name TEXT,
+    category_id INTEGER,
+    category_name TEXT,
+    days_remaining FLOAT,
+    last_update DATETIME
+    
+    );""")
+
+
+def get_type_names(df):
+    df.issued = pd.to_datetime(df.issued)
+    df.drop(columns='type_name', inplace=True)
+    type_ids = df.type_id.unique()
+    typestr = ','.join(str(i) for i in type_ids)
+
+    query = f"SELECT typeID, typeName FROM invTypes WHERE typeID IN ({typestr})"
+
+    engine = create_engine(sde, echo=True)
+    with engine.connect() as conn:
+        df2 = pd.read_sql_query(query, conn)
+        df2.reset_index(drop=True, inplace=True)
+
+    rn_map = {'typeID':'type_id', 'typeName':'type_name'}
+    df2.rename(columns=rn_map, inplace=True)
+    named_df = pd.merge(df, df2, on='type_id', how='left')
+    return named_df
+
+
+def fetch_from_brazil(selected_items: list):
+    engine = create_engine(turso, echo=True)
+    items = selected_items
+    items_str = ','.join(str(i) for i in items)
+
+    # noinspection SqlDialectInspection
+    stmt = text(f"""
+            SELECT * FROM marketstats 
+            WHERE type_id IN ({items_str})
+        """)
+
+    with Session(engine) as session:
+        result = session.execute(stmt)
+        rows = result.fetchall()
+
+    # noinspection PyTypeChecker
+    df = pd.DataFrame(rows, columns=result.keys())
+    return df
+
+
+def update_history():
+    df = pd.read_csv(new_history)
+    df.date = pd.to_datetime(df.date)
+    data = df.to_dict(orient='records')
+
+    engine = create_engine(turso, echo=True)
+    start = datetime.now()
+
+    with Session(engine) as session:
+        try:
+            mapper = inspect(MarketHistory)
+            session.bulk_insert_mappings(mapper, data)
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            logger.info(f'error: {e}')
+    session.close()
+    finish = datetime.now()
+    history_time = finish - start
+    logger.info(f"history time: {history_time}, rows: {len(data)}")
+
+
+def update_orders():
+    df = pd.read_csv(new_orderscsv)
+    df.issued = pd.to_datetime(df.issued)
+    df = get_type_names(df)
+    df.infer_objects()
+
+    data = df.to_dict(orient='records')
+
+    engine = create_engine(turso, echo=True)
+    start = datetime.now()
+
+    with Session(engine) as session:
+        try:
+            mapper = inspect(MarketOrders)
+            session.bulk_insert_mappings(mapper, data)
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            print(f'error: {e}')
+
+    session.close()
+    finish = datetime.now()
+    orders_time = finish - start
+    logger.info(f"orders time: {orders_time}, rows: {len(data)}")
+
+def update_stats():
+    df = pd.read_csv(new_stats)
+    df.last_update = pd.to_datetime(df.last_update)
+    data = df.to_dict(orient='records')
+
+    start = datetime.now()
+
+    engine = create_engine(turso, echo=True)
+    with Session(engine) as session:
+        try:
+            mapper = inspect(MarketStats)
+            session.bulk_insert_mappings(mapper, data)
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            print(f'error: {e}')
+    session.close()
+    finish = datetime.now()
+    stats_time = finish - start
+    logger.info(f"stats time: {stats_time}, rows: {len(data)}")
+
+def main():
+    parser = argparse.ArgumentParser(description="options for market ESI calls")
+
+    # Add arguments
+    parser.add_argument("--hist",
+                        action="store_true",
+                        help="Refresh history data"
+                        )
+
+    args = parser.parse_args()
+
+    if args.hist:
+        logger.info("Running Brazil update with full history refresh.")
+
+    else:
+        logger.info("Running market update in quick mode, saved history data will be used.")
+
+    logger.info(f"connecting to database: {turso}")
+    engine = create_engine(turso, echo=True)
+    conn = engine.connect()
+    logger.info('removing and recreating tables...')
+    with conn.begin():
+        conn.execute(text("DROP TABLE IF EXISTS marketstats"))
+        conn.execute(text("DROP TABLE IF EXISTS marketorders"))
+        conn.execute(text("DROP TABLE IF EXISTS doctrines"))
+
+        conn.commit()
+    conn.close()
+    Base.metadata.create_all(engine)
+
+    logger.info("starting df update...")
+    logger.info('updating table: orders')
+    update_orders()
+    logger.info('updating table: stats')
+    update_stats()
+
+    if args.hist:
+        logger.info('updating table: history')
+        update_history()
+    else:
+        logger.info("df update complete")
+
+    logger.info("syncing embedded replica and closing database connection...")
+
+    engine.dispose()
+
+    conn = libsql.connect('wcmkt.db', sync_url=url, auth_token=auth_token)
+    conn.sync()
+
+    logger.info("sync complete. closing database connection and exiting program.")
+
+
+if __name__ == "__main__":
+    df = pd.read_csv(new_doctrines)
+    df = df.drop_duplicates(subset=["ship_name", "type_name", "group_name", "category_name"])
+    df = df.sort_values(by="timestamp", ascending=False)
+    df = df.reset_index(drop=True)
+    ts = df.loc[0, "timestamp"]
+    df['timestamp'] = df['timestamp'].apply(lambda x: ts if x == 0 else x)
+    df.to_csv(new_doctrines, index=False)
+    print(df.dtypes)
